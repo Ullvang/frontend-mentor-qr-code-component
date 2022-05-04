d3699e9cf801456bb22b8f594717f091707793c8
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2022-05-04 18:52:11 +0200

    done

--- App.tsx
+++ App.tsx
@@ -1,20 +1,134 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import * as WebBrowser from "expo-web-browser";
+import AppLoading from "expo-app-loading";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  Image,
+  StatusBar as SBRN,
+  Dimensions,
+} from "react-native";
+import {
+  useFonts,
+  Outfit_400Regular,
+  Outfit_700Bold,
+} from "@expo-google-fonts/outfit";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const _linkToFrontendMentor = async () => {
+    WebBrowser.openBrowserAsync("https://www.frontendmentor.io?ref=challenge");
+  };
+  const _linkToMyGitHub = async () => {
+    WebBrowser.openBrowserAsync("https://github.com/Ullvang");
+  };
+
+  let [fontsLoaded] = useFonts({
+    Outfit_400Regular,
+    Outfit_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <SafeAreaView style={[styles.container, styles.AndroidSafeArea]}>
+        <View style={styles.card}>
+          <Image
+            style={styles.imageQRCode}
+            source={require("./assets/images/image-qr-code.png")}
+          />
+          <View style={styles.cardText}>
+            <Text style={styles.title}>
+              Improve your front-end skills by building projects
+            </Text>
+            <Text style={styles.paragraph}>
+              Scan the QR code to visit Frontend Mentor and take your coding
+              skills to the next level
+            </Text>
+          </View>
+        </View>
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>
+            Challenge by{" "}
+            <Text onPress={_linkToFrontendMentor} style={styles.link}>
+              Frontend Mentor
+            </Text>
+            .{"\n"}Coded by{" "}
+            <Text onPress={_linkToMyGitHub} style={styles.link}>
+              Jon Erik Ullvang
+            </Text>
+            .
+          </Text>
+        </View>
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    );
+  }
 }
 
+const paddingContainer = 24;
+const padding = paddingContainer * 2 + 16 * 2;
+const win = Dimensions.get("window");
+
 const styles = StyleSheet.create({
+  AndroidSafeArea: {
+    paddingTop: Platform.OS === "android" ? SBRN.currentHeight : 0,
+  },
   container: {
+    padding: paddingContainer,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "hsl(212, 45%, 89%)",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  card: {
+    backgroundColor: "hsl(0, 0%, 100%)",
+    padding: 16,
+    paddingBottom: 32,
+    borderRadius: 16,
+    maxWidth: 375 - paddingContainer * 2,
+    minHeight: 500,
+  },
+  cardText: {
+    paddingHorizontal: 16,
+  },
+  footer: {
+    position: "absolute",
+    bottom: 8,
+  },
+  footerText: {
+    textAlign: "center",
+    lineHeight: 24,
+    color: "hsl(218, 44%, 22%)",
+    fontFamily: "Outfit_400Regular",
+  },
+  imageQRCode: {
+    width: win.width - padding,
+    height: win.width - padding,
+    maxWidth: 375 - padding,
+    maxHeight: 375 - padding,
+    borderRadius: 8,
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: "700",
+    textAlign: "center",
+    marginVertical: 16,
+    color: "hsl(218, 44%, 22%)",
+    fontFamily: "Outfit_700Bold",
+  },
+  paragraph: {
+    fontSize: 15,
+    textAlign: "center",
+    color: "hsl(220, 15%, 55%)",
+    fontFamily: "Outfit_400Regular",
+  },
+  link: {
+    textDecorationLine: "underline",
+    color: "#3e52a3",
+    fontFamily: "Outfit_400Regular",
   },
 });
---
+++ README.md
@@ -0,0 +1,15 @@
+# This project was created with React Native Web and TypeScript
+
+I used React Native for this project because it is a technology I want to learn.
+The site was developed as mobile first.
+
+# Frontend Mentor - QR code component
+
+This is a solution to the ![Design preview for the QR code component coding challenge](./design/desktop-preview.jpg). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+See more about this challange at [https://www.frontendmentor.io/challenges/qr-code-component-iux_sIO_H](https://www.frontendmentor.io/challenges/qr-code-component-iux_sIO_H)
+
+## Author
+
+- Website - [Jon Erik Ullvang](https://jonerikullvang.no)
+- Frontend Mentor - [@ullvang](https://www.frontendmentor.io/profile/ullvang)
+- CSS Battle - [@ullvang](https://cssbattle.dev/player/ullvang)
